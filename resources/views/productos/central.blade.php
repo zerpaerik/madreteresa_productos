a13fe78c10e6ec68dcf3536602a9ff6fb4c1f6b9
zerpaerik <zerpaerik@gmail.com>
2021-05-31 17:19:05 -0400

    pdf en alma central

--- resources/views/productos/central.blade.php
+++ resources/views/productos/central.blade.php
@@ -10,6 +10,9 @@
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+
   <!-- Tempusdominus Bbootstrap 4 -->
   <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
@@ -24,6 +27,8 @@
   <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
   <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+
+
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 <!-- DataTables -->
@@ -68,13 +73,13 @@
     <!-- /.content-header -->
 
     <!-- Main content -->
-    <section class="content">
+    <section class="content">   
     @include('flash-message')
-
       <div class="container-fluid">
       <div class="card">
               <div class="card-header">
                
+                     
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -108,11 +113,7 @@
                    
                   </tr>
                   @endforeach
-                 
-                 
-               
-                 
-                 
+
                   </tbody>
                   <tfoot>
                   <tr>
@@ -136,29 +137,6 @@
         <!-- /.row -->
       </div>
       <!-- /.container-fluid -->
-
-      <div class="modal fade" id="myModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Detalle de Ingreso de Productos</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <p>One fine body&hellip;</p>
-            </div>
-            <div class="modal-footer justify-content-between">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-            </div>
-          </div>
-          <!-- /.modal-content -->
-        </div>
-        <!-- /.modal-dialog -->
-      </div>
-
-     
     </section>
     <!-- /.content -->
   </div>
@@ -177,26 +155,6 @@
 
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
-
-<script type="text/javascript">
-
-function view(e){
-        var id = $(e).attr('data-id');
-        
-        $.ajax({
-            type: "GET",
-            url: "ingresos/view/"+id,
-            success: function (data) {
-                $(".modal-body").html(data);
-                $('#myModal').modal('show');
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
-        });
-    };
-
-</script>
 <!-- jQuery UI 1.11.4 -->
 <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -235,13 +193,20 @@ function view(e){
 <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
 <script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
 <!-- page script -->
-
 <script>
+
 $(document).ready(function() {
     $('#example').DataTable( {
         dom: 'Bfrtip',
@@ -251,11 +216,16 @@ $(document).ready(function() {
     } );
 } );
 </script>
+
 <script>
   $(function () {
     $("#example1").DataTable({
       "responsive": true,
       "autoWidth": false,
+      dom: 'Bfrtip',
+      buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
     });
     $('#example2').DataTable({
       "paging": true,
